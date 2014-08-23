963596205ef1d829e712029be51c2c2c44c1ba51
Dlotan <florian.groetzner@gmail.com>
2014-08-23 16:52:00 -0700

    clean up code3

--- static/js/realtime-access.js
+++ static/js/realtime-access.js
@@ -2,26 +2,14 @@
 
 var rtaccess = rtaccess || {};
 
-/*
- * This function is called the first time that the Realtime model is created
- * for a file. This function should be used to initialize any values of the
- * model. In this case, we just create the single string model that will be
- * used to control our text box. The string has a starting value of 'Hello
- * Realtime World!', and is named 'text'.
- * @param model {gapi.drive.realtime.Model} the Realtime root model object.
- */
+// This function is called the first time that the Realtime model is created
+// for a file.
 rtaccess.initializeModel = function(model) {
   var sharedString = model.createString('print "hello world"');
   model.getRoot().set('code', sharedString);
 }
 
-/**
- * This function is called when the Realtime file has been loaded. It should
- * be used to initialize any user interface components and event handlers
- * depending on the Realtime model. In this case, create a text control binder
- * and bind it to our string model that we created in initializeModel.
- * @param doc {gapi.drive.realtime.Document} the Realtime document.
- */
+// This function is called when the Realtime file has been loaded.
 rtaccess.onFileLoaded = function(doc) {
   var string = doc.getModel().getRoot().get('code');
   
@@ -85,61 +73,36 @@ rtaccess.afterAuthFunc = function() {
   document.getElementById('loader-gif').style.display = "block";
 }
 
-/**
- * Options for the Realtime loader.
- */
+// Options for the Realtime loader.
 rtaccess.realtimeOptions = {
-  /**
-   * Client ID from the console.
-   */
+  // Client ID from the console.
   clientId: '351900000929-isdo9c2d86thju282siblvgug7mm1egg.apps.googleusercontent.com',
 
-  /**
-   * The ID of the button to click to authorize. Must be a DOM element ID.
-   */
+  // The ID of the button to click to authorize. Must be a DOM element ID.
   authButtonElementId: 'authorizeButton',
 
-  /**
-   * Function to be called when a Realtime model is first created.
-   */
+  // Function to be called when a Realtime model is first created.
   initializeModel: rtaccess.initializeModel,
 
-  /**
-   * Autocreate files right after auth automatically.
-   */
+  //Autocreate files right after auth automatically.
   autoCreate: true,
 
-  /**
-   * The name of newly created Drive files.
-   */
+  // The name of newly created Drive files.
   defaultTitle: "PyEditor-File",
 
-  /**
-   * The MIME type of newly created Drive Files. By default the application
-   * specific MIME type will be used:
-   *     application/vnd.google-apps.drive-sdk.
-   */
+  // The MIME type of newly created Drive Files. By default the application
+  // specific MIME type will be used:
+  //     application/vnd.google-apps.drive-sdk.
   newFileMimeType: null, // Using default.
 
-  /**
-   * Function to be called every time a Realtime file is loaded.
-   */
   onFileLoaded: rtaccess.onFileLoaded,
 
-  /**
-   * Function to be called to inityalize custom Collaborative Objects types.
-   */
+  // Function to be called to inityalize custom Collaborative Objects types.
   registerTypes: null, // No action.
 
-  /**
-   * Function to be called after authorization and before loading files.
-   */
   afterAuth: rtaccess.afterAuthFunc
 }
 
-/**
- * Start the Realtime loader with the options.
- */
 rtaccess.startRealtime = function() {
   var realtimeLoader = new rtclient.RealtimeLoader(rtaccess.realtimeOptions);
   realtimeLoader.start();
